f3e3fd632b3dd764dcd8b75ab501ad008c7e1d72
zhangchao <passion888168@126.com>
2019-10-20 20:01:19 +0800

    first commit

---
+++ src/main/java/com/learn/servlet/springbootservlet/SpringbootservletApplication.java
@@ -0,0 +1,13 @@
+package com.learn.servlet.springbootservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootservletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootservletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/servlet/springbootservlet/SpringbootservletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.servlet.springbootservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootservletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
